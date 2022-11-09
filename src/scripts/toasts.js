f1e7300b03b936bd112b10019b497bf8f8d994d2
Tulio Albuquerque <tulioalbuquerque@outlook.com>
2022-11-09 15:32:56 -0300

    fix: login toast

---
+++ src/scripts/toasts.js
@@ -0,0 +1,13 @@
+export const showToast = (status, message) => {
+  const _toastWrapper = document.querySelector("#toast")
+  const _toast = document.querySelector(".toast")
+  const _message = document.querySelector(".toast-message")
+  _message.innerText = message
+  _toast.classList.add(status)
+  _toastWrapper.classList.add("show-toast")
+}
+
+export const hideToast = () => {
+  const _toastWrapper = document.querySelector("#toast")
+  _toastWrapper.classList.remove("show-toast")
+}
